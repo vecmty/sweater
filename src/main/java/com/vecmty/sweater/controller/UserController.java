7a648a4fe940036b74d9bf8efbc753a368f71072
Artur Kamaldinov <ar2r1993@gmail.com>
2021-01-04 05:46:37 +0600

    загрузка файлов / не отдает цвет шрифта

--- src/main/java/com/vecmty/sweater/controller/UserController.java
+++ src/main/java/com/vecmty/sweater/controller/UserController.java
@@ -23,15 +23,17 @@ public class UserController {
     private UserRepo userRepo;
 
     @GetMapping
-    public String userList(Model model){
+    public String userList(Model model) {
         model.addAttribute("users", userRepo.findAll());
+
         return "userList";
     }
 
     @GetMapping("{user}")
-    public String userEditForm(@PathVariable User user, Model model){
+    public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
+
         return "userEdit";
     }
 
@@ -39,22 +41,24 @@ public class UserController {
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
-            @RequestParam("user_id") User user){
+            @RequestParam("userId") User user
+    ) {
         user.setUsername(username);
+
         Set<String> roles = Arrays.stream(Role.values())
                 .map(Role::name)
                 .collect(Collectors.toSet());
 
         user.getRoles().clear();
 
-        for (String key : form.keySet()){
+        for (String key : form.keySet()) {
             if (roles.contains(key)) {
-
                 user.getRoles().add(Role.valueOf(key));
             }
         }
 
         userRepo.save(user);
+
         return "redirect:/user";
     }
 }
