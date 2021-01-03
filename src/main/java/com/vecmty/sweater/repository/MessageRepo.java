57e011c151c10fe7f5fab9a20b836d014547872c
Artur Kamaldinov <ar2r1993@gmail.com>
2021-01-04 01:54:09 +0600

    Регистрация и отображение сообщений

---
+++ src/main/java/com/vecmty/sweater/repository/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.vecmty.sweater.repository;
+
+import com.vecmty.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
---
+++ src/main/java/com/vecmty/sweater/repository/UserRepo.java
@@ -0,0 +1,8 @@
+package com.vecmty.sweater.repository;
+
+import com.vecmty.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
