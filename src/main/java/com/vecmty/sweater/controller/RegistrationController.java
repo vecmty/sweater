708efc08809ad7d91c4be0425e422b346578dac1
Artur Kamaldinov <ar2r1993@gmail.com>
2021-01-11 03:56:19 +0600

    перенес в другое место и не запускается прога + добавил авторизацию по почте

--- src/main/java/com/vecmty/sweater/controller/RegistrationController.java
+++ src/main/java/com/vecmty/sweater/controller/RegistrationController.java
@@ -1,21 +1,21 @@
 package com.vecmty.sweater.controller;
 
-import com.vecmty.sweater.domain.Role;
 import com.vecmty.sweater.domain.User;
-import com.vecmty.sweater.repository.UserRepo;
+import com.vecmty.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
 
     @Autowired
-    UserRepo userRepo;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration(){
@@ -24,16 +24,23 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model){
-        User userFromDb = userRepo.findByUsername(user.getUsername());
-
-        if (userFromDb != null){
+        if (!userService.addUser(user)){
             model.put("message", "User exists!");
             return "registration";
         }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
         return "redirect:/login";
+    }
 
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code){
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated){
+            model.addAttribute("message", "User successfully activated");
+        } else {
+            model.addAttribute("message", "Activation code is not found");
+        }
+
+        return "login";
     }
 }
--- target/classes/templates/registration.ftl
+++
@@ -1,8 +0,0 @@
-<#import "parts/common.ftl" as c>
-<#import "parts/login.ftl" as l>
-
-<@c.page>
-    <div class="mb-1">Add new user</div>
-    ${message?if_exists}
-    <@l.login "/registration" true />
-</@c.page>
